537efb2b067a973e964a76e4423147c24a6b669a
Difroy <unifroy@gmail.com>
2024-09-25 12:11:38 +0200

    Update TropicanoAPI.java

--- src/main/java/com/generation/tropico/api/TropicanoAPI.java
+++ src/main/java/com/generation/tropico/api/TropicanoAPI.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.generation.tropico.model.entities.Tropicano;
 import com.generation.tropico.model.repository.TropicanoRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/tropico/api/tropicanos")
 public class TropicanoAPI {
 
