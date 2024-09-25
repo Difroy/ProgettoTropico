0fbf6514691304124b66d042b2a3afa633d523ce
Difroy <unifroy@gmail.com>
2024-09-25 09:22:24 +0200

    init

---
+++ src/test/java/com/generation/tropico/TropicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.tropico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TropicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
