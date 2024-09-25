6788248e707486414fe4874bebc23d0a56e24afb
Difroy <unifroy@gmail.com>
2024-09-25 18:17:30 +0200

    Update

---
+++ src/main/java/com/generation/tropico/model/repository/BuildingRepository.java
@@ -0,0 +1,8 @@
+package com.generation.tropico.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.generation.tropico.model.entities.Building;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+
+}
