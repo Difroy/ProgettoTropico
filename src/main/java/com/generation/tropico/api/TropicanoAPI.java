1efb080c7d99c3c858160f948fd8122273b3101f
Difroy <unifroy@gmail.com>
2024-09-26 13:56:34 +0200

    Update TropicanoAPI.java

--- src/main/java/com/generation/tropico/api/TropicanoAPI.java
+++ src/main/java/com/generation/tropico/api/TropicanoAPI.java
@@ -1,9 +1,10 @@
 
 package com.generation.tropico.api;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.tropico.model.entities.Education;
+import com.generation.tropico.model.entities.Gender;
+import com.generation.tropico.model.entities.Party;
+import com.generation.tropico.model.dto.TropicanoDTO;
+import com.generation.tropico.model.dto.TropicanoDTOMapper;
 import com.generation.tropico.model.entities.Tropicano;
 import com.generation.tropico.model.repository.TropicanoRepository;
 
@@ -29,73 +35,112 @@ public class TropicanoAPI {
     @Autowired
     private TropicanoRepository tropicanoRepository;
 
+    @Autowired
+    private TropicanoDTOMapper tropicanoDTOMapper;
+    
+    
+    
     // GET all Tropicanos
     @GetMapping
-    public List<Tropicano> getAllTropicanos() {
-        return tropicanoRepository.findAll();
+    public List<TropicanoDTO> getAllTropicanos() {
+        List <Tropicano> tropicanos = tropicanoRepository.findAll();
+        return tropicanos.stream()
+                .map(tropicanoDTOMapper::toDTO)
+                .collect(Collectors.toList());
+        
     }
 
     // GET Tropicano by ID
     @GetMapping("/{id}")
-    public ResponseEntity<Tropicano> getTropicanoById(@PathVariable int id) {
+    public ResponseEntity<TropicanoDTO> getTropicanoById(@PathVariable int id) {
         Optional<Tropicano> tropicano = tropicanoRepository.findById(id);
-        return tropicano.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-    // POST (create) new Tropicano
-    @PostMapping("/all")
-    public ResponseEntity<List<Tropicano>> createTropicanos(@RequestBody List <Tropicano> tropicanos) {
-       
-    	//ALL OR NONE
-    	
-    	List<Tropicano> notValid = new ArrayList<>();
-   	
-    	for(Tropicano tropicano:tropicanos)
-			if (!tropicano.isValid())
-				notValid.add(tropicano);
-    	
-    	if(notValid.size()>0)
-				return ResponseEntity.badRequest().body(tropicanos);
-		return ResponseEntity.ok(tropicanoRepository.saveAll(tropicanos));
+        return tropicano.map(t -> ResponseEntity.ok(tropicanoDTOMapper.toDTO(t)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
     
     @PostMapping
-    public ResponseEntity<Tropicano> createTropicano(@RequestBody Tropicano tropicano) {
+    public ResponseEntity<TropicanoDTO> createTropicano(@RequestBody TropicanoDTO tropicanoDTO) {
        
-    	if(!tropicano.isValid())
-    		return ResponseEntity.badRequest().build();
-    	
-    	return ResponseEntity.ok(tropicanoRepository.save(tropicano));
+Tropicano tropicano = tropicanoDTOMapper.toEntity(tropicanoDTO);
+        
+        // Validate before saving
+        if (!tropicano.isValid()) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        Tropicano savedTropicano = tropicanoRepository.save(tropicano);
+        return ResponseEntity.ok(tropicanoDTOMapper.toDTO(savedTropicano)); 	
     }
+    // POST (create) new Tropicano
+    
+    @PostMapping("/all")
+    public ResponseEntity<List<TropicanoDTO>> createTropicanos(@RequestBody List<TropicanoDTO> tropicanoDTOs) {
+        List<Tropicano> tropicanos = tropicanoDTOs.stream()
+                .map(tropicanoDTOMapper::toEntity)
+                .collect(Collectors.toList());
+
+        // Validate before saving
+        List<Tropicano> notValid = tropicanos.stream()
+                .filter(t -> !t.isValid())
+                .collect(Collectors.toList());
+
+        if (!notValid.isEmpty()) {
+            return ResponseEntity.badRequest().body(
+                    notValid.stream()
+                            .map(tropicanoDTOMapper::toDTO)
+                            .collect(Collectors.toList())
+            );
+        }
+        List<Tropicano> savedTropicanos = tropicanoRepository.saveAll(tropicanos);
+        return ResponseEntity.ok(savedTropicanos.stream()
+                .map(tropicanoDTOMapper::toDTO)
+                .collect(Collectors.toList())); 
+         
+    }
+    
+    
+   
+    
+    
     // PUT (update) Tropicano
     @PutMapping("/{id}")
-    public ResponseEntity<Tropicano> updateTropicano(@PathVariable int id, @RequestBody Tropicano tropicanoDetails) {
-        Optional<Tropicano> optionalTropicano = tropicanoRepository.findById(id);
-        if (optionalTropicano.isPresent()) {
-            Tropicano tropicanoToUpdate = optionalTropicano.get();
-            tropicanoToUpdate.setName(tropicanoDetails.getName());
-            tropicanoToUpdate.setSurname(tropicanoDetails.getSurname());
-            tropicanoToUpdate.setBirthyear(tropicanoDetails.getBirthyear());
-            tropicanoToUpdate.setEducation(tropicanoDetails.getEducation());
-            tropicanoToUpdate.setGender(tropicanoDetails.getGender());
-            tropicanoToUpdate.setParty(tropicanoDetails.getParty());
-            Tropicano updatedTropicano = tropicanoRepository.save(tropicanoToUpdate);
-            return ResponseEntity.ok(updatedTropicano);
+    public ResponseEntity<TropicanoDTO> updateTropicano(@PathVariable int id, @RequestBody TropicanoDTO tropicanoDTO) {
+        Optional<Tropicano> tropicanoOptional = tropicanoRepository.findById(id);
+
+        if (tropicanoOptional.isPresent()) {
+            Tropicano tropicano = tropicanoOptional.get();
+            tropicano.setName(tropicanoDTO.getName());
+            tropicano.setSurname(tropicanoDTO.getSurname());
+            tropicano.setBirthyear(tropicanoDTO.getBirthyear());
+            tropicano.setGender(Gender.valueOf(tropicanoDTO.getGender()));
+            tropicano.setEducation(Education.valueOf(tropicanoDTO.getEducation()));
+            tropicano.setParty(Party.valueOf(tropicanoDTO.getParty()));
+            tropicano.setSatisfaction(tropicanoDTO.getSatisfaction());
+            tropicano.setImage(tropicanoDTO.getImage());
+
+            Tropicano updatedTropicano = tropicanoRepository.save(tropicano);
+            return ResponseEntity.ok(tropicanoDTOMapper.toDTO(updatedTropicano));
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.notFound().build();
         }
     }
+    
+    
+    
+    
+    
+    
 
     // DELETE Tropicano
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTropicano(@PathVariable int id) {
         Optional<Tropicano> tropicano = tropicanoRepository.findById(id);
+
         if (tropicano.isPresent()) {
             tropicanoRepository.delete(tropicano.get());
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            return ResponseEntity.noContent().build();
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.notFound().build();
         }
     }
 }
